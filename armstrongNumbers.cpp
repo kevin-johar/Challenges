f282c8749deebc5fc1c22b52f16c25da2dd119a9
Kevin Johar <kevin@dyn172-30-230-240.reznet.wireless.uwo.pri>
2018-03-01 00:28:56 -0500

    Armstrong Numbers Program

--- armstrongNumbers.cpp
+++ armstrongNumbers.cpp
@@ -7,12 +7,40 @@
 //
 
 #include <iostream>
-using std::cout;
-using std::cin;
-using std::endl;
+#include <cmath>
+using namespace std;
 
 int main() {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    char loop = 'y';
+    
+    cout<<"An Armstrong number is one where the sum of each digit's cube is the number itself"<<endl;
+    cout<<"Ex. 153, 1^3 + 5^3 + 3^3 = 1 + 125 + 27 = 153\n"<<endl;
+    
+    while (!(loop=='n')) {
+        int input,mod(0),temp(0),hold;
+        cout<<"I will calculate the Armstrong Number of any integer!"<<endl;
+        cout<<"Please input an integer: ";
+        cin>>input;
+
+        hold = input;
+        
+        for (;input!=0; input/=10) {
+            mod = input%10;
+            temp=temp+(pow(mod,3));
+        }
+        
+        if (hold==temp) {
+            cout<<"\nThis is an armstrong number!\n"<<endl;
+        }
+        
+        else {
+            cout<<"\nThis is not an armstrong number!\n"<<endl;
+        }
+        
+        cout<<"Would you like to try again? (y/n):";
+        cin>>loop;
+        cout<<"\n";
+    }
+    
     return 0;
 }
