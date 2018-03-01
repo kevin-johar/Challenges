165c554c4cabfcb10c61f87d5cea93618f32f9f9
Kevin Johar <kevin@dyn172-30-230-240.reznet.wireless.uwo.pri>
2018-03-01 00:34:54 -0500

    Added comments to the armstrongNumbers.cpp

--- armstrongNumbers.cpp
+++ armstrongNumbers.cpp
@@ -11,32 +11,43 @@
 using namespace std;
 
 int main() {
+    //Char variable to control exit out of the program, dictated by the user
     char loop = 'y';
     
+    //Explanation of an Armstrong number
     cout<<"An Armstrong number is one where the sum of each digit's cube is the number itself"<<endl;
     cout<<"Ex. 153, 1^3 + 5^3 + 3^3 = 1 + 125 + 27 = 153\n"<<endl;
     
+    //While loop to repeat program until user exits
     while (!(loop=='n')) {
+        //Variable declaration
         int input,mod(0),temp(0),hold;
+        
+        //Recieves user input
         cout<<"I will calculate the Armstrong Number of any integer!"<<endl;
         cout<<"Please input an integer: ";
         cin>>input;
 
+        //Holds the user input for comparison later
         hold = input;
         
+        /*For loop using modular arithmetic to find the sum of each digit's cubes for the inputted number*/
         for (;input!=0; input/=10) {
             mod = input%10;
             temp=temp+(pow(mod,3));
         }
         
+        //If the original number is equal to the calculated sum of the digit's cubes, armstrong num
         if (hold==temp) {
             cout<<"\nThis is an armstrong number!\n"<<endl;
         }
         
+        //Otherwise not an armstrong num
         else {
             cout<<"\nThis is not an armstrong number!\n"<<endl;
         }
         
+        //Asks the user if they would want to try the program again
         cout<<"Would you like to try again? (y/n):";
         cin>>loop;
         cout<<"\n";
